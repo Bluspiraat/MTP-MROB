fe058d6627450c5d60f2479fbf5f5e4f23790799
Matthijs Sluijk <s2277093@linux629.itc.utwente.nl>
2025-11-10 14:50:18 +0100

    added tracking of CCE loss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datasets import RGBDataset, RGBDSMDataset
 from datasets.augmentations import get_image_net_normalization, get_rgb_transform, get_geometric_transform
 from torch.utils.data import DataLoader, random_split, Subset, ConcatDataset
-from models import RGBUNet
+from models import RGBUNet, EarlyFusionUNet
 from train import metrics
 import torch
 from train.loss import ComboLoss
@@ -13,58 +13,65 @@ from tqdm import tqdm
 if __name__ == '__main__':
 
     # Load data
-    train_folder = "C:/MTP-Data/dataset_diverse_2022_512_sep/train"
-    val_folder = "C:/MTP-Data/dataset_diverse_2022_512_sep/val"
+    train_folder = "/home/s2277093/MTP-Data/dataset_diverse_2022_512_sep/train"
+    val_folder = "/home/s2277093/MTP-Data/dataset_diverse_2022_512_sep/val"
 
-    num_epochs = 20
+    num_epochs = 75
     best_val_loss = float('inf')
     patience_counter = 0
-    patience_limit = 5
-    improvement_threshold = 1e-3
-    save_name = "rgb_unet_varied_full"
+    patience_limit = 30
+    improvement_threshold = 1e-4
+    save_name = "rgbdsm_u_net_r34_b16_a03"
     subset_size = 10000
-    batch_size = 4
-    alpha = 0.5
-    learning_rate = 1e-4
+    batch_size = 16
+    alpha = 0.3
+    learning_rate = 1e-3    
+    scheduler_temp = 50
 
     # Load dataset
-    train_set = RGBDataset(rgb_dir=f'{train_folder}/ortho/',
-                           mask_dir=f'{train_folder}/brt/',
-                           normalization=get_image_net_normalization(),
-                           geo_transform=get_geometric_transform(),
-                           rgb_transform=get_rgb_transform())
-    val_set = RGBDataset(rgb_dir=f'{val_folder}/ortho/',
-                         mask_dir=f'{val_folder}/brt/',
-                         normalization=get_image_net_normalization())
+    train_set = RGBDSMDataset(rgb_dir=f'{train_folder}/ortho/',
+                            dsm_dir=f'{train_folder}/dsm/',
+                            mask_dir=f'{train_folder}/brt/',
+                            normalization=get_image_net_normalization(),
+                            geo_transform=get_geometric_transform(),
+                            rgb_transform=get_rgb_transform())
+    val_set = RGBDSMDataset(rgb_dir=f'{val_folder}/ortho/',
+                            dsm_dir=f'{val_folder}/dsm/',
+                            mask_dir=f'{val_folder}/brt/',
+                            normalization=get_image_net_normalization())
 
     # Dataloaders
-    train_batches = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=2, pin_memory=True)
-    val_batches = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=2, pin_memory=True)
+    train_batches = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=8, prefetch_factor=2, pin_memory=True)
+    val_batches = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=8, prefetch_factor=2, pin_memory=True)
 
     # Setup model
-    model = RGBUNet()
+    model = EarlyFusionUNet(pretrained=False)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:1")  # use second GPU
     model.to(device)
 
     criterion = ComboLoss(num_classes=14, alpha=alpha)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=scheduler_temp, eta_min=1e-5)
 
     history = {
         'train_loss': [],
         'val_loss': [],
-        'val_dice': []
+        'val_dice': [],
+        'val_cce': []
     }
 
     try:
         for epoch in range(num_epochs):
             tqdm.write(f"\nEpoch {epoch + 1}/{num_epochs}")
             train_loss = train_one_epoch(model, train_batches, criterion, optimizer, device)
-            val_loss, val_dice = validate_one_epoch(model, val_batches, criterion, device, num_classes=14)
+            val_loss, val_dice, val_cce = validate_one_epoch(model, val_batches, criterion, device, num_classes=14)
+            scheduler.step()
             history['train_loss'].append(train_loss)
             history['val_loss'].append(val_loss)
             history['val_dice'].append(val_dice)
-            tqdm.write(f"\nEpoch {epoch + 1}: Train Loss={train_loss:.4f}, Val Loss={val_loss:.4f}, Val Dice={val_dice:.4f}")
+            history['val_cce'].append(val_cce)
+            tqdm.write(f"\nEpoch {epoch + 1}: Train Loss={train_loss:.4f}, Val Loss={val_loss:.4f}, Val Dice={val_dice:.4f}, Val Dice={val_cce:.4f}")
 
             if best_val_loss - val_loss > improvement_threshold:
                 best_val_loss = val_loss
--- train/validate.py
+++ train/validate.py
@@ -4,9 +4,13 @@ from .metrics import dice_score_multiclass
 
 
 def validate_one_epoch(model, dataloader, criterion, device, num_classes):
+
+    cce_criterion = nn.CrossEntropyLoss().to(device)
+
     model.eval()
     running_loss = 0.0
     running_dice = 0.0
+    running_cce = 0.0
 
     with torch.no_grad():
         loop = tqdm(dataloader, desc="Validation", leave=False)
@@ -18,9 +22,13 @@ def validate_one_epoch(model, dataloader, criterion, device, num_classes):
             loss = criterion(logits, mask)
             running_loss += loss.item()
 
+            cce_loss = cce_criterion(logits, mask)
+            running_cce += cce_loss.item()
+
             dice = dice_score_multiclass(logits, mask, num_classes)
             running_dice += dice
 
     avg_loss = running_loss / len(dataloader)
     avg_dice = running_dice / len(dataloader)
-    return avg_loss, avg_dice
+    avg_cce =  running_cce / len(dataloader)
+    return avg_loss, avg_dice, avg_cce
