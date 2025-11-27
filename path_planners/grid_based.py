c22031dc28223a261bcf9000364dab43e20b9a70
Matthijs Sluijk <matthijs.sluijk@gmail.com>
2025-11-27 12:07:36 +0100

    Refactoring of the path planner, it now uses heap datastructure to improve search time of a path.

--- path_planners/grid_based.py
+++ path_planners/grid_based.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List, Dict, Tuple, Set
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,17 +17,24 @@ class AStarOccupancy:
         self.start = start
         self.goal = goal
 
-        # --- Setup score tracking and node lists --- #
-        open_set = [start]                                              # Possible nodes to check
-        closed_set: Set[Tuple[int, int]] = set()                        # Set of nodes with fully optimized paths
-        f_scores = [self._heuristic(start)]                             # The priority value of nodes
+        open_heap = []
+        open_set = set()
+
         g_scores: Dict[Tuple[int, int], int] = {start: 0}               # Cost to travel to a certain node
         came_from: Dict[Tuple[int, int], Tuple[int, int]] = {}          # The parent node of a give node
 
+        f_start = self._heuristic(start)
+        heapq.heappush(open_heap, (f_start, start))
+        open_set.add(start)                                             # Possible nodes to check
+        closed_set: Set[Tuple[int, int]] = set()                        # Set of nodes with fully optimized paths
+
         time_start = time.time()                                        # Track starting time of the search
-        while open_set:
-            idx = int(np.argmin(f_scores))                              # Pick value with the lowest priority score
-            current = open_set[idx]
+        while open_heap:
+            f_current, current = heapq.heappop(open_heap)               # Retrieves pair with the lowest f-score
+
+            # Skip outdated entries --> Node might be in the closed set, but not popped from the heap
+            if current in closed_set:
+                continue
 
             # If current node is the goal node
             if current == goal:
@@ -35,9 +43,8 @@ class AStarOccupancy:
                 return self.path
 
             # Move current into closed set
+            open_set.remove(current)
             closed_set.add(current)
-            open_set.pop(idx)
-            f_scores.pop(idx)
 
             # Evaluate all neighbours of current node
             for neighbour in self.grid.get_neighbours(current):
@@ -57,18 +64,15 @@ class AStarOccupancy:
                 # if the node has no associated cost or the cost reduced, then the node should be updated.
                 if g_old is None or tentative_g < g_old:
                     g_scores[neighbour] = tentative_g
-                    f_score: int = tentative_g + self._heuristic(neighbour)
                     came_from[neighbour] = current
 
-                    # Situation: The node has not been seen before and is assigned a priority value based on the cost to
-                    # get to the node and the heuristic
-                    if neighbour not in open_set:
-                        open_set.append(neighbour)
-                        f_scores.append(f_score)
-                    # Situation: The existence of the node is known and its priority value should be updated.
-                    else:
-                        neighbour_index = list.index(open_set, neighbour)
-                        f_scores[neighbour_index] = f_score
+                    f_score: int = tentative_g + self._heuristic(neighbour)
+
+                    # It might occur that a node exists multiple times in the heap with different f-scores.
+                    # Re-processing of a node is avoided by the check if the node is in the closed set after selecting
+                    # a new current node
+                    heapq.heappush(open_heap, (f_score, neighbour))
+                    open_set.add(neighbour)  # Adding does not matter, if it is already in there, it will be overwritten
 
         print('No path found')
         return None
